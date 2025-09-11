a343e8b14dced0c9f26330d86701e4a2641b44a4
Chesere <chessm755@gmail.com>
2025-09-11 22:23:10 +0300

    Created and styled the toggle menu

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -10,6 +10,7 @@ import {
   Mail,
   Users,
   Bell,
+  Menu,
   Info,
 } from "lucide-react";
 
@@ -25,7 +26,7 @@ const ICONS = {
 };
 const Sidebar = () => {
   const [sidebarItems, setSidebarItems] = useState([]);
-
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const pathname = usePathname();
 
   useEffect(() => {
@@ -35,8 +36,18 @@ const Sidebar = () => {
   }, []);
 
   return (
-    <div className="relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 w-64">
+    <div
+      className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${
+        isSidebarOpen ? "w-64" : "w-20"
+      }`}
+    >
       <div className="h-full bg-[#1e1e1e] backdrop-blur-md p-4 flex flex-col border-r border-[#2f2f2f] ">
+        <button
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="p-2 rounded-full hover:bg-[#2f2f2f] transition-colors max-w-fit cursor-pointer"
+        >
+          <Menu size={24} />
+        </button>
         <nav className="mt-8 flex-grow">
           {sidebarItems.map((item) => {
             const IconComponent = ICONS[item.icon] || Info;
@@ -48,7 +59,9 @@ const Sidebar = () => {
                   }`}
                 >
                   <IconComponent size={20} style={{ minWidth: "20px" }} />
-                  <span className="ml-4 whitespace-nowrap">{item.name}</span>
+                  {isSidebarOpen && (
+                    <span className="ml-4 whitespace-nowrap">{item.name}</span>
+                  )}
                 </div>
               </Link>
             );
