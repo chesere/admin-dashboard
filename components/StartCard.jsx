4aef9afa4ff639bcfc81f129c1baa2d68a4c7a25
Chesere <chessm755@gmail.com>
2025-09-12 23:10:15 +0300

    Creating and styling statcards

--- components/StartCard.jsx
+++ components/StartCard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const StartCard = () => {
-  return <div>StartCard</div>;
+const StartCard = ({ name, icon: Icon, value }) => {
+  return (
+    <div className="bg-[#1e1e1e] backdrop-blur-md overflow-hidden shadow-md rounded-xl border border-[#1f1f1f]">
+      <div className="px-4 py-5 sm:p-6">
+        <span className="flex items-center text-sm font-medium text-gray-300">
+          <Icon size={20} className="mr-2" />
+          {name}
+        </span>
+        <p className="mt-1 text-3xl font-semibold text-white">{value}</p>
+      </div>
+    </div>
+  );
 };
 
 export default StartCard;
