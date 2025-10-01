eb39017c44caf398be7a3798767af0f4921c14b3
Chesere <chessm755@gmail.com>
2025-10-01 20:16:50 +0300

    Applying the animation effect

--- components/OrderDistributionChart.jsx
+++ components/OrderDistributionChart.jsx
@@ -8,6 +8,7 @@ import {
   ResponsiveContainer,
   Tooltip,
 } from "recharts";
+import { motion } from "framer-motion";
 
 const COLORS = ["#FBC02D", "#03a9f4", "#EF4444", "#8bc34a"];
 
@@ -21,7 +22,12 @@ const OrderDistributionChart = () => {
   }, []);
 
   return (
-    <div className="bg-[#1e1e1e] backdrop-blur-md shadow-lg rounded-xl p-4 md:p-6 border border-[#1f1f1f] mx-2 md:mx-0">
+    <motion.div
+      className="bg-[#1e1e1e] backdrop-blur-md shadow-lg rounded-xl p-4 md:p-6 border border-[#1f1f1f] mx-2 md:mx-0"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.3, duration: 0.5 }}
+    >
       <h2 className="text-base md:text-xl font-semibold text-gray-100 mb-4 text-center md:text-left">
         Order Status Distribution
       </h2>
@@ -66,7 +72,7 @@ const OrderDistributionChart = () => {
           </PieChart>
         </ResponsiveContainer>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
