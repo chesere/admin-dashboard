0b77db42568af6539324fd640aecd32ee4dc6d1c
Chesere <chessm755@gmail.com>
2025-10-01 19:11:03 +0300

    Created the distribution pie chart for order status

--- components/OrderDistributionChart.jsx
+++ components/OrderDistributionChart.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Pie, PieChart, ResponsiveContainer } from "recharts";
+
+const COLORS = ["#FBC02D", "#03a9f4", "#EF4444", "#8bc34a"];
 
 const OrderDistributionChart = () => {
-  return <div>OrderDistributionChart</div>;
+  const [orderStatusData, setOrderStatusData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/data.json")
+      .then((res) => res.json())
+      .then((data) => setOrderStatusData(data.orderStatus));
+  }, []);
+
+  return (
+    <div className="bg-[#1e1e1e] backdrop-blur-md shadow-lg rounded-xl p-4 md:p-6 border border-[#1f1f1f] mx-2 md:mx-0">
+      <h2 className="text-base md:text-xl font-semibold text-gray-100 mb-4 text-center md:text-left">
+        Order Status Distribution
+      </h2>
+      <div className="w-full h-64 md:h-80">
+        <ResponsiveContainer>
+          <PieChart>
+            <Pie
+              data={orderStatusData}
+              cx="50%"
+              cy="50%"
+              outerRadius={70}
+              dataKey="value"
+              label={({ name, percent }) =>
+                `${name} ${(percent * 100).toFixed(0)}%`
+              }
+            ></Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 };
 
 export default OrderDistributionChart;
