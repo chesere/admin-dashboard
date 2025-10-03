57388a7a55ef00a6b5efafdc8b14b4f0de920e3c
Chesere <chessm755@gmail.com>
2025-10-03 13:16:08 +0300

    Created the searchFunction

--- components/ProductsTable.jsx
+++ components/ProductsTable.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import productData from "../public/data/data.json";
 import { motion } from "framer-motion";
 import { Edit, Search, Trash2 } from "lucide-react";
@@ -8,6 +8,19 @@ import Image from "next/image";
 
 const ProductsTable = () => {
   const [products, setProducts] = useState(productData.products);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredProducts = useMemo(() => {
+    return products.filter(
+      (product) =>
+        product.name
+          .toLocaleLowerCase()
+          .includes(searchTerm.toLocaleLowerCase()) ||
+        product.category
+          .toLocaleLowerCase()
+          .includes(searchTerm.toLocaleLowerCase())
+    );
+  });
   return (
     <motion.div
       className="bg-[#1e1e1e] backdrop-blur-md shadow-lg rounded-xl p-4 md:p-6 border border-[#1f1f1f] mx-2 md:mx-0 mb-8"
@@ -22,6 +35,8 @@ const ProductsTable = () => {
         <div className="relative w-full md:w-auto">
           <input
             type="text"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
             placeholder="Search Products....."
             className="bg-[#2f2f2f] text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 w-full md:w-64 focus:outline-none focus:ring-2 focus:ring-gray-500 transition duration-200 text-sm "
           />
@@ -51,7 +66,7 @@ const ProductsTable = () => {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-700">
-            {products.map((product) => (
+            {filteredProducts.map((product) => (
               <motion.tr
                 key={product.id}
                 initial={{ opacity: 0, y: 10 }}
