6f1ccaf5451b0301727716db442d64862d30b513
Chesere <chessm755@gmail.com>
2025-10-03 13:03:47 +0300

    Created the list of products

--- components/ProductsTable.jsx
+++ components/ProductsTable.jsx
@@ -2,7 +2,9 @@
 import React, { useState } from "react";
 import productData from "../public/data/data.json";
 import { motion } from "framer-motion";
-import { Search } from "lucide-react";
+import { Edit, Search, Trash2 } from "lucide-react";
+import { div, td } from "framer-motion/client";
+import Image from "next/image";
 
 const ProductsTable = () => {
   const [products, setProducts] = useState(productData.products);
@@ -48,6 +50,95 @@ const ProductsTable = () => {
               ))}
             </tr>
           </thead>
+          <tbody className="divide-y divide-gray-700">
+            {products.map((product) => (
+              <motion.tr
+                key={product.id}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.1, duration: 0.3 }}
+                className="flex flex-col md:table-row md:mb-0 border-b md:border-b-0 border-gray-700 md:border-none p-2 md:p-0 "
+              >
+                <td className="md:hidden px-3 py-2">
+                  <div className="flex items-center justify-between ">
+                    <div className="flex items-center">
+                      <Image
+                        src={product.image}
+                        alt={product.name}
+                        width={36}
+                        height={36}
+                        className="w-9 h-9 rounded-full"
+                      />
+                      <div className="ml-3">
+                        <div className="text-sm font-medium text-gray-100">
+                          {product.name}
+                        </div>
+                        <div className="text-xs text-gray-400">
+                          ID:{product.id}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex space-x-1 -mt-1 -mr-1">
+                      <button className="text-indigo-500 hover:text-indigo-300">
+                        <Edit size={16} />
+                      </button>
+                      <button className="text-red-500 hover:text-red-300">
+                        <Trash2 size={16} />
+                      </button>
+                    </div>
+                  </div>
+                  <div className="mt-2 text-xs text-gray-300">
+                    <div> Category:{product.category}</div>
+                    {["price", "stock", "sales"].map((field) => (
+                      <div key={field}>
+                        <span className="capitalize">
+                          {field}:{product[field]}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                </td>
+                <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-100">
+                  <div className="flex items-center">
+                    <Image
+                      src={product.image}
+                      alt={product.name}
+                      width={40}
+                      height={40}
+                      className="w-10 h-10 rounded-full"
+                    />
+                    <div className="ml-4">{product.name}</div>
+                  </div>
+                </td>
+                <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                  {product.id}
+                </td>
+                <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                  {product.category}
+                </td>
+                {["price", "stock", "sales"].map((field) => (
+                  <td
+                    key={field}
+                    className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm text-gray-300"
+                  >
+                    {field === "price"
+                      ? `$${product[field].toFixed(2)}`
+                      : product[field]}
+                  </td>
+                ))}
+                <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                  <div className="flex space-x-1 -ml-2">
+                    <button className="text-indigo-500 hover:text-indigo-300 mr-1 cursor-pointer">
+                      <Edit size={18} />
+                    </button>
+                    <button className="text-red-500 hover:text-red-300 cursor-pointer">
+                      <Trash2 size={18} />
+                    </button>
+                  </div>
+                </td>
+              </motion.tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </motion.div>
